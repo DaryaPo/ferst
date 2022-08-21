22ee23dff449553614203afa263f6e69919154ed
Darya <datya@mail.ru>
2022-08-21 20:35:56 +0300

    решение первой задачи

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int x;
+int y;
+Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите число: ");
+x = Convert.ToInt32(Console.ReadLine());
+y = Convert.ToInt32(Console.ReadLine());
+if (x>y)
+{
+    Console.WriteLine("Max " + x);
+    Console.WriteLine("Min " + y);
+}
+else
+{
+Console.WriteLine("Max " + y);
+    Console.WriteLine("Min " + x);
+}
+
+
+
+
+
+
+
+
+
